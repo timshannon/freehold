aca0423c8a1b64096f8faf4f7503b21dcc77d14f
Tim Shannon <shannon.timothy@gmail.com>
2014-05-16 16:58:52 -0500

    Added selfsign flag to generate a self signed cert.

--- main.go
+++ main.go
@@ -1,14 +1,31 @@
 package main
 
 import (
+	"flag"
 	"net/http"
 
 	"bitbucket.org/tshannon/config"
 )
 
-//selfsign
+const (
+	defaultCertFile = "cert.pem"
+	defaultKeyFile  = "key.pem"
+)
+
+var selfSign bool = false
+
+func init() {
+	flag.Var(&selfSign, "selfsign", "Generate a self-signed certificate. "+
+		"If "+defaultCertFile+", or "+defaultKeyFile+"already exists, they will not be overridden.")
+}
 
 func main() {
+	flag.Parse()
+	if selfSign {
+
+		//TODO
+		return
+	}
 	cfg, err := config.LoadOrCreate("settings.json")
 	if err != nil {
 		panic("Error loading settings.json file: " + err.Error())
