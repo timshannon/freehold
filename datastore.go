6fc0fdc501e69e245e54bf6846b68e81a9864113
Tim Shannon <shannon.timothy@gmail.com>
2014-06-03 22:02:19 -0500

    work on core datastore handling

--- datastore.go
+++ datastore.go
@@ -1,6 +1,11 @@
 package main
 
-import "bitbucket.org/tshannon/freehold/datastore"
+import (
+	"encoding/json"
+	"os"
+
+	"bitbucket.org/tshannon/freehold/datastore"
+)
 
 type FHIter struct {
 	From   interface{} `json:"from,omitempty"`
@@ -9,25 +14,44 @@ type FHIter struct {
 	Order  string      `json:"order,omitempty"`
 	Limit  int         `json:"limit,omitempty"`
 	Regexp string      `json:"regexp,omitempty"`
+
+	ds datastore.Datastore
 }
 
-type FHDSResult struct {
-	Key   interface{} `json:"key,omitempty"`
-	Value interface{} `json:"value,omitempty"`
+type KeyValue struct {
+	Key   json.RawMessage
+	Value json.RawMessage
 }
 
 func openCoreDS(filename string) datastore.Datastore {
 	ds, err := datastore.Open(filename)
-	if io.IsNotExist(err) {
+	if os.IsNotExist(err) {
 		err = datastore.Create(filename)
 		if err != nil {
 			//TODO: Log error
+			return nil
 		}
 		ds, err = datastore.Open(filename)
 	}
 
 	if err != nil {
 		//TODO: Log error
+		return nil
 	}
 	return ds
+}
+
+// m is an internal only shortcut for marshalling an arbitrary value.
+// It will panic if it can't be marshalled
+func m(key interface{}) []byte {
+	buf, err := json.Marshal(key)
+	if err != nil {
+		panic("Error marshalling key: " + err.Error())
+	}
+	return buf
+}
+
+// iter returns a json []byte result of the iterater
+func (i *FHIter) iter() []byte {
+
 }
--- settings.go
+++ settings.go
@@ -2,11 +2,21 @@ package main
 
 const settingDS = "core/settings.ds"
 
-type Setting struct {
+type FHSetting struct {
 	Description string      `json:"description,omitempty"`
 	Value       interface{} `json:"value,omitempty"`
 }
 
 func Setting(setting string) interface{} {
 	ds := openCoreDS(settingDS)
+	var value []byte
+	err := ds.Get(m(setting), value)
+	if err != nil {
+		//TODO: Log error
+	}
+
+	if value == nil {
+		//Lookup default value
+
+	}
 }
