1b66049d34e843dd442f2d2bfb5e401f7e6b7d9a
Tim Shannon <tim@townsourced.com>
2014-06-25 21:46:17 -0500

    missed readding errors.go

---
+++ errors.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"errors"
+	"io"
+	"net/http"
+	"os"
+)
+
+const (
+	default404Path = "/" + version + "/file/core/404.html"
+)
+
+func errHandled(err error, w http.ResponseWriter) bool {
+	if err == nil {
+		return false
+	}
+
+	status, errMsg := errorMessage(err)
+	if status == statusFail {
+		w.WriteHeader(http.StatusBadRequest)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+	}
+	respondJsend(w, &JSend{
+		Status:  status,
+		Message: errMsg,
+	})
+	return true
+}
+
+func four04(w http.ResponseWriter, r *http.Request) {
+	if settingBool("Log404") {
+		logError(errors.New("Resource not found: " + r.URL.String()))
+	}
+
+	prm, err := permissions(settingString("404File"))
+	if err != nil {
+		logError(err)
+		http.NotFound(w, r)
+		return
+	}
+	if !prm.canRead(nil) {
+		logError(errors.New("The 404File setting is pointing at a non-public file: " +
+			settingString("404File")))
+		http.NotFound(w, r)
+		return
+	}
+
+	file, err := os.Open(urlPathToFile(settingString("404File")))
+	defer file.Close()
+	if err != nil {
+		logError(err)
+		http.NotFound(w, r)
+		return
+	}
+
+	info, err := file.Stat()
+	if err != nil {
+		logError(err)
+		http.NotFound(w, r)
+		return
+	}
+
+	w.WriteHeader(http.StatusNotFound)
+	//Have to do this manually so 404 Status Code is preserved
+	io.CopyN(w, file, info.Size())
+}
