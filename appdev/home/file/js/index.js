9ddeceb8c01ab1ae59d1017c045f37553e3452d4
Tim Shannon <shannon.timothy@gmail.com>
2014-07-23 16:50:54 -0500

    Add doc for license data and attribution.  Fiddled a bit with ractive stuff in the home app.

--- appdev/home/file/js/index.js
+++ appdev/home/file/js/index.js
@@ -1,34 +1,48 @@
 $(document).ready(function() { 
-	$("#logoutButton").click(function() {
-		fh.logout();
-		window.location = "/";
-	});
-	
-	var installed = new Ractive({
-		el: "#installed",
-		template: "#appGrid"
-	});
 
-	var available = new Ractive({
-		el: "#available",
-		template: "#appGrid"
-	});
+$("#logoutButton").click(function() {
+	fh.logout();
+	window.location = "/";
+});
 
+var available = new Ractive({
+	template: "#appModal"
+});
 
-	fh.application.installed()
-	.always(function(result) {
-		installed.set(result);
-	});
+var installed = new Ractive({
+	el: "#installed",
+	template: "#appGrid"
+});
 
-	fh.application.available()
-	.always(function(result) {
-		available.set(result);
-	});
+var modal = new Ractive({
+	el: "#available",
+	template: "#appGrid"
+});
 
 
-	//TODO: figure out which available apps are already installed
-	// check version numbers to see if they are upgrades
-	
+fh.application.installed()
+.always(function(result) {
+	installed.set(result);
+});
+
+fh.application.available()
+.always(function(result) {
+	result.modal = true;
+	available.set(result);
+});
+
+//TODO: figure out which available apps are already installed
+// check version numbers to see if they are upgrades
+
+available.on({
+	openModal: function(event) {
+		//open modal and set ractive data to the current app
+		console.log(event);
+	}
+});
 
 }); //end ready
+
+
+
 
