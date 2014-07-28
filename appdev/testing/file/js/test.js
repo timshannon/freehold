0985a71f279757bce92239dbf712b1d564db2cad
Tim Shannon <shannon.timothy@gmail.com>
2014-07-27 22:45:36 -0500

    Little bit of work on the testing app

--- appdev/testing/file/js/test.js
+++ appdev/testing/file/js/test.js
@@ -1,3 +1,13 @@
-QUnit.test("Properties", function(assert) {
+QUnit.asyncTest("Properties", function(assert) {
+	expect(1);
+	var valid = {
+		status:"success",
+		data: ""
+	};
 
+	fh.properties.get("/testing/v1/properties/file/data/testfile.txt")
+	.always(function(result) {
+		assert.deepEqual(result, valid);
+		QUnit.start();
+	});
 });
