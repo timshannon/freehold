62d50c378bd820c881aa0c1587c0f785085ad194
Tim Shannon <shannon.timothy@gmail.com>
2014-08-27 10:29:38 -0500

    Removed randomized lock file, since it was a stupid idea, and just removed the lock file entirely.  See lock.go for details on my reasoning.

--- data/store/lock.go
+++ data/store/lock.go
@@ -2,68 +2,45 @@
 // Use of this source code is governed by the MIT license
 // that can be found in the LICENSE file.
 
-// Same as default KV locker, except with a randmized lockfile name
-// resolves an issue where multiple go routines create and delete
-// a ds file with the same name.  It shouldn't conflict, because
-// the RW mux should prevent it, but occasionally I get lock file
-// exists errors.  This clears it up.
+// default locker is for cross-process locking via a file
+// Freehold is only single process and the lock file actually
+// seems to be causing issues with concurrent requests to create
+// datastores
+// The freehold locker does nothing instead
+
+// TODO: The default locker may be used if I can find a satisfactory method
+// of handling simulaneous requests to create or delete the same datastore
+// Possible options:
+//	Marked for Deletion - Mark a datastore for deletion instead of
+//		deleting right away, return a failure to anyone trying
+//		to read or write a datastore marked for deletion
+//	Drop Timeout - Set datastore deletion on a timeout
+//		and if any reads or writes are made on a datastore
+//		that has been set to be dropped, then cancel the
+//		drop request and leave the datastore as is.
+
+// I don't like either option.  The first gives worse concurrency
+// than we currently have with the lockfiles
+// The second is unexpected behahvior. "I dropped this datastore, why
+// is it still around?"
+// In the end this scenario of creating and dropping the same datastore on
+// concurrent threads doesn't seem to be a realistic scenario outside of
+// the Unit Tests, as only a DS owner can create or drop a DS.
+// However if you have multiple applications, all running as you
+// with separate tokens, that want to create or drop the same DS
+// They may run into an issue.  So currently I'm sticking with no lock file for now.
 
 package store
 
-import (
-	"crypto/rand"
-	"fmt"
-	"io"
-	"os"
-	"path/filepath"
-	"sync"
-)
-
-func lockname(filename string) string {
-	base := filepath.Base(filepath.Clean(filename))
-
-	result := make([]byte, 4)
-	_, err := io.ReadFull(rand.Reader, result)
-	if err != nil {
-		panic(fmt.Sprintf("Error generating random values: %v", err))
-	}
-
-	return filepath.Join(filepath.Dir(filename), fmt.Sprintf(".%x.%s", result, base))
-}
+import "io"
 
 func freeholdLocker(dbname string) (io.Closer, error) {
-	lname := lockname(dbname)
-	abs, err := filepath.Abs(lname)
-	if err != nil {
-		return nil, err
-	}
-	f, err := os.OpenFile(abs, os.O_CREATE|os.O_EXCL|os.O_RDONLY, 0666)
-	if os.IsExist(err) {
-		return nil, fmt.Errorf("cannot access DB %q: lock file %q exists", dbname, abs)
-	}
-	if err != nil {
-		return nil, err
-	}
-	return &lockCloser{f: f, abs: abs}, nil
+	return &lockCloser{}, nil
 }
 
 type lockCloser struct {
-	f    *os.File
-	abs  string
-	once sync.Once
-	err  error
 }
 
 func (lc *lockCloser) Close() error {
-	lc.once.Do(lc.close)
-	return lc.err
-}
-
-func (lc *lockCloser) close() {
-	if err := lc.f.Close(); err != nil {
-		lc.err = err
-	}
-	if err := os.Remove(lc.abs); err != nil {
-		lc.err = err
-	}
+	return nil
 }
