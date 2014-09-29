aa224946e8a064ff39315b1033f80bb14db40c2d
Tim Shannon <tim@townsourced.com>
2014-09-28 22:45:15 -0500

    More messing around with getting the components build process to work.

--- appdev/components/components.dev.js
+++ appdev/components/components.dev.js
@@ -1,28 +1,14 @@
 //Freehold ractive components 
 // for things like modals, navbar, etc
-
-//FIXME: ForceSync?  Everyone else seems to wrap start and end fragments
-// this seems to work fine for now
-
-require.config({
-    paths: {
-        ractive: "../../application/core/file/js/ractive.min",
-		rvc: "./rvc"
-    }
-});
-
-
-require(['rvc!modal'], function(Modal) {
-    Ractive.components.modal = Modal;
-}, null, true);
-
-
-
-require(['rvc!navbar'], function(Navbar) {
-    Ractive.components.navbar = Navbar;
-}, null, true);
-
-
-
 //TODO: filetree
 //TODO: permissions
+
+
+    require(['rvc!modal'], function(Modal) {
+        Ractive.components.modal = Modal;
+    });
+
+
+    require(['rvc!navbar'], function(Navbar) {
+        Ractive.components.navbar = Navbar;
+    });
